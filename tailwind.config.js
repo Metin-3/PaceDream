afde78c7fa51da6b25e452b970232eaa63ed4b56
matin-mib <matinhasanov.mibtech@gmail.com>
2025-03-14 19:59:40 +0400

    fix: add folder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        colorPrimary: "rgba(85 39 215)"
+      }
+    },
+  },
+  plugins: [],
+}
